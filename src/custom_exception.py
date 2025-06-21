d1b803ca0a0cbe4ea8456ddb82e1b5e41187f2b5
saksham011023 <msaksham1016@gmail.com>
2025-06-22 00:29:51 +0530

    Clean slate: remove large files

---
+++ src/custom_exception.py
@@ -0,0 +1,20 @@
+import traceback
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message) # if the exception is already in the Exception class this method automatically handles it without creating our own CustomExceptions.
+
+        self.error_message=self.get_detailed_error_message(error_message,error_detail)
+
+    @staticmethod
+    def get_detailed_error_message(error_message,error_detail:sys):
+        _,_,exc_tb=error_detail.exc_info()  # it return 3 values but we are only concerned with the last tracback value.
+        file_name=exc_tb.tb_frame.f_code.co_filename
+        line_number=exc_tb.tb_lineno
+
+        return f"Error occured in {file_name} , Line {line_number} : {error_message}"
+    
+    def __str__(self): #gives text format of error message.
+        return self.error_message
---
+++ utils/common_functions.py
@@ -0,0 +1,25 @@
+import os
+import pandas
+from src.logger import get_logger
+from src.custom_exception import CustomException
+import yaml
+
+logger=get_logger(__name__)
+
+"""
+A code to read the YAML file during data ingestion or data preprocessing.
+"""
+def read_yaml(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("File is not in the given path")
+        
+        with open(file_path,"r") as yaml_file:
+            config=yaml.safe_load(yaml_file)
+            logger.info("Successfully loaaded the YAML file")
+            return config
+        
+    except Exception as e:
+        logger.error("Error while reading the YAML file.")
+        raise CustomException("Failed to read YAML file.",e)
+    
